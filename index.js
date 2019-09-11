6f6b17faff52fb3959a4d2465ea8126bf03909d7
Leo Kalshteyn <leo.kalshteyn@gmail.com>
2019-09-11 03:08:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Code your solutions in this file
+function countdown() {
+  let keepWorking = true;
+  while (keepWorking) {
+    chooseSeedLocation();
+    plantSeed();
+    waterSeed();
+    keepWorking = checkForMoreSeeds();
+  }
+}
