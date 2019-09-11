1a66fb4aa8354253944cc9032df8257ae3526bba
Leo Kalshteyn <leo.kalshteyn@gmail.com>
2019-09-11 03:15:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ function writeCards(names, events){
   const thankdCards = [];
   for (let i = 0; i < names.length; i++) {
     thankYou.push("Thank you, " + names[i] + ", for the wonderful " + events  + " gift!");
+  }
 
+  return thankYou;
 }
-
 
 
 
